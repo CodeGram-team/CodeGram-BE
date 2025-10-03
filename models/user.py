8ed0b9a0b5c8915ba4b2d7d36810fcdd540d57bc
tlsnwls <tlsdnwls0103@gmail.com>
2025-10-03 18:27:54 +0900

    chore: id type change

--- models/user.py
+++ models/user.py
@@ -8,14 +8,14 @@ import uuid
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, nullable=False)
-    nickname = Column(String, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4())
+    email = Column(String, unique=True, index=True, nullable=False)
+    nickname = Column(String, unique=True, index=True, nullable=False)
     username = Column(String, nullable=False)
     profile_image_url = Column(String, nullable=True)
-    created_at = Column(DateTime, default=text('now()'))
+    created_at = Column(DateTime(timezone=True), server_default=text('now()'))
     
-    social_accounts = relationship("SocialAccounts", back_populates="user", cascade="all, delete-orphan")
+    social_accounts = relationship("SocialAccount", back_populates="user", cascade="all, delete-orphan")
 
 class SocialAccount(Base):
     __tablename__ = "social_accounts"
