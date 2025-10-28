9763f832e088d5613bfa38fe568993b70ef255d7
tlsnwls <tlsdnwls0103@gmail.com>
2025-10-29 04:13:07 +0900

    no commit msg

--- db/database.py
+++ db/database.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 DATABASE_URL=os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL)
-AsnycSessionLocal = sessionmaker(
+
+AsyncSessionLocal = sessionmaker(
     engine, 
     class_=AsyncSession,
     expire_on_commit=False
@@ -14,5 +15,7 @@ AsnycSessionLocal = sessionmaker(
 Base = declarative_base()
 
 async def get_db():
-    async with AsnycSessionLocal() as session: # with 구문이 끝나면 세션이 자동으로 close/rollback 됨
+    async with AsyncSessionLocal() as session: # with 구문이 끝나면 세션이 자동으로 close/rollback 됨
         yield session
+        
+# TODO: Connection Pool and session management 
