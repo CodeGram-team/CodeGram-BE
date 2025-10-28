f75c7afd56b4af418a30d475b737df8683d4cd9b
tlsnwls <tlsdnwls0103@gmail.com>
2025-10-29 04:07:51 +0900

    Feat: Added regex

--- schema/google_oauth.py
+++ schema/google_oauth.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+import re
 
 class GoogleLoginRequest(BaseModel):
     id_token:str
     
 class SignUpCompletionRequest(BaseModel):
-    signup_token:str
-    nickname:str
+    signup_token: str
+    nickname: str
+
+    @field_validator('nickname')
+    def nickname_valid(cls, v):
+        if not 3 <= len(v) <= 10:
+            raise ValueError("닉네임은 3~10자리여야 합니다")
+        if not re.match(r'^[가-힣a-zA-Z0-9_]+$', v):
+            raise ValueError("닉네임은 한글, 영어, 숫자, _만 허용됩니다")
+        return v
