429d0baeb1e5a687add92a66f91074767181377b
tlsnwls <tlsdnwls0103@gmail.com>
2025-10-29 04:12:08 +0900

    added Lifespan

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import uvicorn 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from db.mongodb import init_db, close_connection
 from db.rmq import rmq_client
 from api.v1.auth import oauth_router
 from api.v1.execution import execution_router
+from api.v1.post import post_router
+from api.v1.challenge import challenge_router
+from api.v1.profile import profile_router
 
 async def start():
     await init_db()
@@ -26,9 +30,23 @@ app = FastAPI(title="CodeGram API",
               description="API for CodeGram Service",
               lifespan=lifespan)
 
+origins = [
+    "https://codesns.cloudjin.kr" # 나중에 실제 배포될 프론트엔드 주소
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # ["*"] 대신 명시적인 리스트 사용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(oauth_router)
 app.include_router(execution_router)
+app.include_router(post_router)
+app.include_router(challenge_router)
+app.include_router(profile_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
