6524602ef6efb956b362be6a58b6765891d67d5e
tlsnwls <tlsdnwls0103@gmail.com>
2025-11-02 01:48:38 +0900

    Feat: add refresh token request schema

--- schema/token.py
+++ schema/token.py
@@ -4,6 +4,7 @@ from typing import Optional
 class TokenResponse(BaseModel):
     access_token:str
     refresh_token:str
+    expires_time: int
     token_type:str="bearer"
     
 class GoogleLoginResponse(BaseModel):
@@ -11,4 +12,8 @@ class GoogleLoginResponse(BaseModel):
     access_token: Optional[str] = None
     refresh_token: Optional[str] = None
     signup_token: Optional[str] = None
+    expires_time: Optional[int] = None
     token_type: str = "bearer"
+    
+class RefreshTokenRequest(BaseModel):
+    refresh_token:str
